903909b3d645e803e1b8eecb2f9b78db2c81cc70
João Ornellas <65920201+jo4o0rn3ll4s@users.noreply.github.com>
2023-05-31 13:09:24 -0300

    refactor

---
+++ Entradas_e_saidas/iostream/cin.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int valor;
+	
+	cout << "Digite um numero: ";
+	cin >> valor;
+	
+	cout << endl << "O valor digitado foi " << valor;
+}
---
+++ Entradas_e_saidas/iostream/teste_cin.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int num;
+	float deci;
+	
+	cout << "Escreva um numero inteiro, e um numero decimal: ";
+	cin >> num >> deci;
+	cout << "valor que entrou foi " << num << " e " << deci << endl;
+	
+	char let;
+	
+	cout << "Escreva uma letra: ";
+	cin >> let;
+	cout << "a letra escrita foi " << let;
+}
