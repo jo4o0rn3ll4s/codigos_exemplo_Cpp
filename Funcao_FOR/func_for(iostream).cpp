d0a04e5962380bc117c057cd94184af5f796741b
João Ornellas <65920201+jo4o0rn3ll4s@users.noreply.github.com>
2023-05-31 16:09:16 -0300

    Add files via upload

---
+++ Funcao_FOR/func_for(iostream).cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int rep;
+	
+	//recolhe o dado do numero de vezes para repeticao
+	cout << "Escreve o numero de repeticoes desejadas \n\n";
+	cin >> rep;
+	
+	//cria uma variavel de contar e a compara com a variavel anterior
+	for(int i = 0; i < rep; i++){
+		cout << "Irei repetir essa frase " << rep << " vezes.";
+		cout << "Ja escrevi " << i+1 << " vezes\n\n";
+	//pos executar, soma 1 a variavel de contagem e volta ao inicio
+	}
+	//chega aqui pos a funcao retornar como falso a condicao
+}
+
+
